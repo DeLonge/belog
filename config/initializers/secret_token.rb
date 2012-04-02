9a73c703b89451ab2766bfda0c042b34e17bf486
DeLonge <emotionz_punk@yahoo.co.id>
2012-04-02 21:07:44 +0700

    Adding login plugin with Devise

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bratha::Application.config.secret_token = '85dc010dbad306734e78115e9c1f8685d0adfb24207049da106552227bcdf7943fa39663ae1d5dd74e110fe0c6915a46578b68616b70c4952986e4df0ec5e35f'
